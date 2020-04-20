4a2a1abe9909e51f63ad154bf559a3a931eeda08
Isaac Nam <ikn208@nyu.edu>
2020-04-20 12:08:46 -0400

    finished splash page

---
+++ frontend/src/components/loading/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Loading extends React.Component {
+    render() {
+        return (
+           <h1>Loading component</h1>
+        )
+    }
+}
+
+export default Loading;
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -6,10 +6,16 @@ class Splash extends React.Component {
     render() {
         return (
             <div className="splash-container">
-                <h1>Enter a username</h1>
-                <input className="splash-input-username" type="text" name="" id=""/>
-                <button className="splash-play-button">Play</button>
+                <div className="splash-game-title">
+                    <h1>DECEPTION</h1>
+                </div>
+                <div className="splash-form">
+                    <h1>Enter a username</h1>
+                    <input className="splash-input-username" type="text" placeholder="Bob Jenkins"/>
+                    <button className="splash-play-button">Play</button>
+                </div>
             </div>
+            
         )
     }
 }
