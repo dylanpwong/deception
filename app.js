282ee221c9d67d91f8bb36950d699b63523c1cc5
dpwong <dylanpwong@gmail.com>
2020-04-20 10:10:44 -0400

    models made

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const User = require('./models/User')
-const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+////////
+const User = require('./models/User')
+const users = require('./routes/api/users');
 //useUnifiedTopology: true
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch((err) => console.log(err));
 
-// app.use(bodyParser.urlencoded({
-//     extended: false
-// }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
+app.get('/',(req,res)=>{
+    const user = new User({
+       username: "player1",
+       role: "Investogator", 
+    })
+    user.save()
+    res.send("User saved!")
+})
+
+app.use("/api/users",users)
 
 
 const port = process.env.PORT || 5000;
---
+++ models/Event.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  Location: {
+    type: String,
+    required: true,
+  },
+
+  causeofDeath: {
+    type: String,
+    required: true,
+  },
+
+  murdersPersonality:{
+      type: String,
+      required: true,
+  },
+
+  noticedByBystander:{
+      type: String,
+      required: true,
+  },
+
+  traceAtTheScene:{
+      type: String,
+      required: true,
+  },
+
+  socialRelationship:{
+      type: String,
+      required: true,
+  },
+
+  victimsIdentiy:{
+      type: String,
+      required: true,
+  },
+
+  stateOfScene:{
+      type: String,
+      required: true
+  },
+
+  inProgress:{
+      type: String,
+      required: true,
+  },
+
+  weather: {
+    type: String,
+    required: true,
+  },
+
+  corpseCondition: {
+      type: String,
+      required: true
+  },
+  
+  hintOnCorpse: {
+      type: String,
+      required: true 
+  },
+
+  motiveOfCrime: {
+      type: String, 
+      required: true
+  },
+
+  durationOfCrime: {
+      type: String, 
+      required: true
+  },
+
+  evidenceLeftBehind: {
+      type: String,
+      required: true
+  },
+
+  victimsClothes: {
+      type: String,
+      required: true
+  },
+
+  suddenIncident: {
+      type: String,
+      required: true
+  },
+  
+  victimsBuild: {
+      type: String,
+      required: true 
+  },
+
+  victimsExpression: {
+      type: String,
+      required: true
+  },
+  
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Event = mongoose.model("events", UserSchema);
+module.exports = Event;
