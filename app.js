f3200d876c5e6de52456d7266626b1f93a41daf1
dpwong <dylanpwong@gmail.com>
2020-04-21 19:02:11 -0400

    heroku build

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+const path = require('path');
 ////////
 const User = require('./models/User')
 const users = require('./routes/api/users');
 const cards = require('./routes/api/cards');
 const events = require('./routes/api/events');
 //useUnifiedTopology: true
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
