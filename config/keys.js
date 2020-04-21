8b879c1f204e1b2ffc5adc84f1a376205c876950
dpwong <dylanpwong@gmail.com>
2020-04-21 18:32:13 -0400

    prod updated

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:j83MD1cDTeQtBtBs@cluster0-mnmem.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secretKey",
-};
+// module.exports = {
+//   mongoURI:
+//     "mongodb+srv://dev:j83MD1cDTeQtBtBs@cluster0-mnmem.mongodb.net/test?retryWrites=true&w=majority",
+//   secretOrKey: "secretKey",
+// };
+// keys.js
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:j83MD1cDTeQtBtBs@cluster0-mnmem.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secretKey",
+};
