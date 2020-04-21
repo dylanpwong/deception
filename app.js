04a7db9f13c6655cde39896c8921d3a031283d22
dpwong <dylanpwong@gmail.com>
2020-04-21 14:40:07 -0400

    give up

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+const WebSocket = require('ws');
+const http = require('http');
+const serv = require('http').Server(app);
 ////////
-const User = require('./models/User')
+const User = require('./models/User');
 const users = require('./routes/api/users');
 const cards = require('./routes/api/cards');
 const events = require('./routes/api/events');
@@ -35,4 +38,32 @@ app.use("/api/events",events);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => { console.log(`Listening on port ${port}`) });
+
+serv.listen(port, () => { console.log(`Listening on port ${port}`) });
+
+// serv.listen(3000);
+ let io = require('socket.io')(serv,{});
+ io.sockets.on('connection',function(socket){
+     console.log("Isaac Connected! ~yay");
+     socket.on('yay',()=>{
+         console.log("Big Yay");
+     })
+ })
+// const server = http.createServer(app);
+
+// const wss = new WebSocket.Server({server});
+
+// wss.on('connection',({ws: WebSocket})=>{
+//     // ws.on('happy',()=>{
+//     //     console.log("Issac");
+//     // })
+//     console.log("connected Isaac!");
+// });
+// var io = require('socket.io')(app,{});
+// io.sokects.on('connection',function(sockets){
+//     console.log('socket connection')
+// })
+
+// socket.on('happy',()=>{
+//     console.log('Isaac');
+// })
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './splash.css';
 import { Link } from 'react-router-dom';
-
+// import io from 'socket.io';
+// import openSocket from "socket.io-client";
+// import wsScript from "http://cdn.socket.io/socket.io-1.4.5.js";
 class Splash extends React.Component {
 
+    socket(){
+        // let socket = io();
+        // console.log("hello2");
+        //<script src="http://cdn.socket.io/socket.io-1.4.5.js"></script>;
+        // let socket = io()
+            
+        // const socket = openSocket();
+         console.log("hello");
+            return (
+              <>
+                <script src="http://cdn.socket.io/socket.io-1.4.5.js"></script>
+                <script>var socket=io();</script>
+              </>
+            );
+
+    }
+
+    componentDidMount() {
+        console.log('splash page :)')
+    }
+
     render() {
-        return (
-            <div className="splash-container">
+            // var socket=io();
+          return (
+              
+              <div className="splash-container">
+              {/* <script src="http://cdn.socket.io/socket.io-1.4.5.js"></script> */}
+                {this.socket.bind(this)()}
                 <Link className="game-title" to="/">DECEPTION</Link>
                 <div className="splash-form">
                     <h1>Enter a username</h1>
